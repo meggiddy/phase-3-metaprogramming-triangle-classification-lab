46e1dca6be7ede04079d5f91c8d9243fcb2950e6
meggidy <mimamitchelle@gmail.com>
2023-02-20 17:39:01 +0300

    Completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
   # write code here
+  class TriangleError < StandardError
+  end
+
+  def initialize(height, width, hypotenus)
+    @sides = [height, width, hypotenus].sort
+    raise TriangleError, "Invalid sides" unless valid?
+  end
+
+  def kind
+    case @sides.uniq.size
+    when 1
+      :equilateral
+    when 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def valid?
+    @sides.all?(&:positive?) && @sides[0] + @sides[1] > @sides[2]
+  end
 end
